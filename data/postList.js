e4310857cdabc05b5023c770295968df7e68895b
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-30 14:33:59 +0100

    add_array_post

--- data/postList.js
+++ data/postList.js
@@ -2,31 +2,34 @@ const listPostEl = [
     {
         "titolo": "La bellezza dei tramonti",
         "contenuto": "I tramonti offrono uno spettacolo naturale che ci lascia senza parole. Ogni sera, il cielo si tinge di colori incredibili.",
-        "immagine": "tramonto.jpg",
+        "immagine": "/img/tramonto.jpeg",
         "tags": ["natura", "fotografia", "tramonto"]
     },
     {
         "titolo": "Ricetta della Pasta alla Carbonara",
         "contenuto": "Scopri come preparare la vera carbonara romana, un piatto semplice ma delizioso.",
-        "immagine": "carbonara.jpg",
+        "immagine": "/img/carbonara.jpeg",
         "tags": ["cucina", "ricette", "italiana"]
     },
     {
         "titolo": "I benefici della meditazione",
         "contenuto": "La meditazione è una pratica antica che porta numerosi benefici alla mente e al corpo.",
-        "immagine": "meditazione.jpg",
+        "immagine": "/img/meditazione.jpeg",
         "tags": ["benessere", "mindfulness", "salute"]
     },
     {
         "titolo": "Top 10 libri da leggere",
         "contenuto": "Ecco una lista dei dieci libri che ogni appassionato di lettura dovrebbe avere nella propria libreria.",
-        "immagine": "libri.jpg",
+        "immagine": "/img/libri.jpeg",
         "tags": ["lettura", "cultura", "libri"]
     },
     {
         "titolo": "Viaggio in Giappone",
         "contenuto": "Un'avventura indimenticabile tra templi, giardini e la cultura unica del Giappone.",
-        "immagine": "giappone.jpg",
+        "immagine": "/img/giappone.jpeg",
         "tags": ["viaggi", "cultura", "Giappone"]
     }
 ]
+
+
+module.exports = listPostEl;
