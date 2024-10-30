5d3fb83153666408a594ddea228390f46c145a1a
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-30 18:16:08 +0100

    no_fix_axios

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
-const blogControllers = require('./controllers/blogControllers.js')
+const blogControllers = require('./controllers/blogControllers')
 
 const host = 'http://127.0.0.1'
 const port = 3000
@@ -13,9 +13,18 @@ app.listen(port, () => {
     
 })
 
-
+app.get('/ciao', (req, res) => {
+    
+    
+    res.send('<img src="/img/carbonara.jpeg" alt="pizza vegana">')
+  })
   
 
 app.get('/blog', blogControllers.index) 
 
 
+app.get('/', (req, res) => {
+  
+    res.sendFile(path.join(__dirname,'public', 'index.html'));
+    
+  })
--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,26 @@
 
 
-const listPost = require('')
+
 const containerEl = document.querySelector('.container')
 
+let listEl= document.querySelector('ul')
 
-
-
-listPost.forEach(element => {
-    console.log(element);
-    const {titolo,contenuto,immagine,tags} = element
-
-    const markup = `
-  
-    <ul>
-        <li>${titolo}</li>
-        <li>${contenuto}</li>
-        <li><img src="${immagine}" alt=""></li>
-        <li>${tags}</li>
-    </ul>
-
-    `
-containerEl.innerHTML += markup
-
+axios.get("http://127.0.0.1:3000/blog")
+.then(res=>{
+    const posts = res.data
+    
+    
+    listElement= ''
+    posts.forEach(post => {
+        let {titolo,contenuto, immagine, tags} = post
+        const markup=`
+            <li class="pb-2 text-center"><h2>${titolo}</h2></li>
+            <li class="pb-2 text-center">${contenuto}</li>
+            <li class="pb-2 text-center"><img src="${immagine}" alt=""></li>
+            <li class="pb-2 text-center">${tags}</li>
+            
+            `
+        listElement+=markup
+    });
+    listEl.innerHTML= listElement
 })
