629e77176b351fac7c1f425a78a94435de0e374b
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-11-04 09:52:28 +0100

    add_id_post

--- data/postList.js
+++ data/postList.js
@@ -2,30 +2,35 @@
 
 const posts = [
     {
+        "id": 1,
         "titolo": "La bellezza dei tramonti",
         "contenuto": "I tramonti offrono uno spettacolo naturale che ci lascia senza parole. Ogni sera, il cielo si tinge di colori incredibili.",
         "immagine": "/img/tramonto.jpeg",
         "tags": ["natura", "fotografia", "tramonto"],
     },
     {
+        "id": 2,
         "titolo": "Ricetta della Pasta alla Carbonara",
         "contenuto": "Scopri come preparare la vera carbonara romana, un piatto semplice ma delizioso.",
         "immagine": "/img/carbonara.jpeg",
         "tags": ["cucina", "ricette", "italiana"]
     },
     {
+        "id": 3,
         "titolo": "I benefici della meditazione",
         "contenuto": "La meditazione è una pratica antica che porta numerosi benefici alla mente e al corpo.",
         "immagine": "/img/meditazione.jpeg",
         "tags": ["benessere", "mindfulness", "salute"]
     },
     {
+        "id": 4,
         "titolo": "Top 10 libri da leggere",
         "contenuto": "Ecco una lista dei dieci libri che ogni appassionato di lettura dovrebbe avere nella propria libreria.",
         "immagine": "/img/libri.jpeg",
         "tags": ["lettura", "cultura", "libri"]
     },
     {
+        "id": 5,
         "titolo": "Viaggio in Giappone",
         "contenuto": "Un'avventura indimenticabile tra templi, giardini e la cultura unica del Giappone.",
         "immagine": "/img/giappone.jpeg",
